39d7bc347cfa1385bbe78cd126d4089ce7c758f9
hanif <list>
2020-07-28 20:21:27 +0700

    tambah gempa.php

---
+++ gempa.php
@@ -0,0 +1,33 @@
+<?php
+
+// impor dan load class BMKGGempaGeoJSON
+require_once('BMKGGempaGeoJSON.php');
+
+use BMKGGempaGeoJSON as BMKG;
+
+$bmkg = new BMKG;
+
+// cek apakah variable $_GET['gempa'] ada atau tidak 
+if (isset($_GET['data'])) {
+    $data = $_GET['data'];
+} else {
+    $data = null;
+}
+
+// presentasi data
+$geojson = null;
+
+switch ($data) {
+    case "m-5-terkini":
+        // Gempa M 5+ Terkini
+        $geojson = $bmkg->getGempaM5Terkini();
+        break;
+    case "tsunami-terkini":
+        // Gempa Berpotensi Tsunami Terkini
+        $geojson = $bmkg->getGempaBerpotensiTsunamiTerkini();
+        break;
+    default:
+        echo "developed by <a href='https://hanifmu.com'><strong>hanifmu.com</strong></a>";
+}
+
+print_r($geojson);
